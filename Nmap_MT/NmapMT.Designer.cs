1be91ca6fe94cc84d694804be4e3e4bb8b2ca077
ashvin.bhuttoo <ashvin.bhuttoo@gmail.com>
2019-10-28 19:06:56 +0400

    first commit

---
+++ Nmap_MT/NmapMT.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace Nmap_MT
+{
+    partial class NmapMT
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFrom1 = new System.Windows.Forms.TextBox();
+            this.txtFrom2 = new System.Windows.Forms.TextBox();
+            this.txtFrom3 = new System.Windows.Forms.TextBox();
+            this.txtFrom4 = new System.Windows.Forms.TextBox();
+            this.txtTo1 = new System.Windows.Forms.TextBox();
+            this.txtTo2 = new System.Windows.Forms.TextBox();
+            this.txtTo3 = new System.Windows.Forms.TextBox();
+            this.txtTo4 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lvScans = new System.Windows.Forms.ListView();
+            this.IP = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Results = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.numThreads = new System.Windows.Forms.NumericUpDown();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tstProgress = new System.Windows.Forms.ToolStripProgressBar();
+            this.tstStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numThreads)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtFrom1
+            // 
+            this.txtFrom1.Location = new System.Drawing.Point(13, 16);
+            this.txtFrom1.MaxLength = 3;
+            this.txtFrom1.Name = "txtFrom1";
+            this.txtFrom1.Size = new System.Drawing.Size(34, 20);
+            this.txtFrom1.TabIndex = 0;
+            this.txtFrom1.Tag = "From Octet 1";
+            this.txtFrom1.Text = "192";
+            this.txtFrom1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.ip_change);
+            // 
+            // txtFrom2
+            // 
+            this.txtFrom2.Location = new System.Drawing.Point(53, 16);
+            this.txtFrom2.MaxLength = 3;
+            this.txtFrom2.Name = "txtFrom2";
+            this.txtFrom2.Size = new System.Drawing.Size(34, 20);
+            this.txtFrom2.TabIndex = 0;
+            this.txtFrom2.Tag = "From Octet 2";
+            this.txtFrom2.Text = "168";
+            this.txtFrom2.KeyUp += new System.Windows.Forms.KeyEventHandler(this.ip_change);
+            // 
+            // txtFrom3
+            // 
+            this.txtFrom3.Location = new System.Drawing.Point(93, 16);
+            this.txtFrom3.MaxLength = 3;
+            this.txtFrom3.Name = "txtFrom3";
+            this.txtFrom3.Size = new System.Drawing.Size(34, 20);
+            this.txtFrom3.TabIndex = 0;
+            this.txtFrom3.Tag = "From Octet 3";
+            this.txtFrom3.Text = "1";
+            this.txtFrom3.KeyUp += new System.Windows.Forms.KeyEventHandler(this.ip_change);
+            // 
+            // txtFrom4
+            // 
+            this.txtFrom4.Location = new System.Drawing.Point(133, 16);
+            this.txtFrom4.MaxLength = 3;
+            this.txtFrom4.Name = "txtFrom4";
+            this.txtFrom4.Size = new System.Drawing.Size(34, 20);
+            this.txtFrom4.TabIndex = 0;
+            this.txtFrom4.Tag = "From Octet 4";
+            this.txtFrom4.Text = "1";
+            this.txtFrom4.KeyUp += new System.Windows.Forms.KeyEventHandler(this.ip_change);
+            // 
+            // txtTo1
+            // 
+            this.txtTo1.Location = new System.Drawing.Point(13, 16);
+            this.txtTo1.MaxLength = 3;
+            this.txtTo1.Name = "txtTo1";
+            this.txtTo1.Size = new System.Drawing.Size(34, 20);
+            this.txtTo1.TabIndex = 0;
+            this.txtTo1.Tag = "To Octet 1";
+            this.txtTo1.Text = "192";
+            this.txtTo1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.ip_change);
+            // 
+            // txtTo2
+            // 
+            this.txtTo2.Location = new System.Drawing.Point(53, 16);
+            this.txtTo2.MaxLength = 3;
+            this.txtTo2.Name = "txtTo2";
+            this.txtTo2.Size = new System.Drawing.Size(34, 20);
+            this.txtTo2.TabIndex = 0;
+            this.txtTo2.Tag = "To Octet 2";
+            this.txtTo2.Text = "168";
+            this.txtTo2.KeyUp += new System.Windows.Forms.KeyEventHandler(this.ip_change);
+            // 
+            // txtTo3
+            // 
+            this.txtTo3.Location = new System.Drawing.Point(93, 16);
+            this.txtTo3.MaxLength = 3;
+            this.txtTo3.Name = "txtTo3";
+            this.txtTo3.Size = new System.Drawing.Size(34, 20);
+            this.txtTo3.TabIndex = 0;
+            this.txtTo3.Tag = "To Octet 3";
+            this.txtTo3.Text = "1";
+            this.txtTo3.KeyUp += new System.Windows.Forms.KeyEventHandler(this.ip_change);
+            // 
+            // txtTo4
+            // 
+            this.txtTo4.Location = new System.Drawing.Point(133, 16);
+            this.txtTo4.MaxLength = 3;
+            this.txtTo4.Name = "txtTo4";
+            this.txtTo4.Size = new System.Drawing.Size(34, 20);
+            this.txtTo4.TabIndex = 0;
+            this.txtTo4.Tag = "To Octet 4";
+            this.txtTo4.Text = "255";
+            this.txtTo4.KeyUp += new System.Windows.Forms.KeyEventHandler(this.ip_change);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtFrom4);
+            this.groupBox1.Controls.Add(this.txtFrom1);
+            this.groupBox1.Controls.Add(this.txtFrom2);
+            this.groupBox1.Controls.Add(this.txtFrom3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(191, 42);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "From";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtTo4);
+            this.groupBox2.Controls.Add(this.txtTo1);
+            this.groupBox2.Controls.Add(this.txtTo2);
+            this.groupBox2.Controls.Add(this.txtTo3);
+            this.groupBox2.Location = new System.Drawing.Point(219, 5);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(191, 42);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "To";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox1);
+            this.groupBox3.Location = new System.Drawing.Point(451, 5);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(328, 42);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Nmap CmdLine Options Override";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(304, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // lvScans
+            // 
+            this.lvScans.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.IP,
+            this.Results});
+            this.lvScans.HideSelection = false;
+            this.lvScans.Location = new System.Drawing.Point(12, 121);
+            this.lvScans.Name = "lvScans";
+            this.lvScans.Size = new System.Drawing.Size(776, 326);
+            this.lvScans.TabIndex = 4;
+            this.lvScans.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.Font = new System.Drawing.Font("Segoe UI Historic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartStop.ForeColor = System.Drawing.Color.Green;
+            this.btnStartStop.Location = new System.Drawing.Point(219, 69);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(560, 34);
+            this.btnStartStop.TabIndex = 5;
+            this.btnStartStop.Text = "Start";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.numThreads);
+            this.groupBox4.Location = new System.Drawing.Point(12, 61);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(191, 42);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Number of Threads";
+            // 
+            // numThreads
+            // 
+            this.numThreads.Location = new System.Drawing.Point(76, 16);
+            this.numThreads.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numThreads.Name = "numThreads";
+            this.numThreads.Size = new System.Drawing.Size(91, 20);
+            this.numThreads.TabIndex = 0;
+            this.numThreads.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tstProgress,
+            this.tstStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 471);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tstProgress
+            // 
+            this.tstProgress.Name = "tstProgress";
+            this.tstProgress.Size = new System.Drawing.Size(500, 16);
+            // 
+            // tstStatus
+            // 
+            this.tstStatus.Name = "tstStatus";
+            this.tstStatus.Size = new System.Drawing.Size(32, 17);
+            this.tstStatus.Text = "Idle..";
+            // 
+            // NmapMT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 493);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnStartStop);
+            this.Controls.Add(this.lvScans);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "NmapMT";
+            this.Text = "NmapMT";
+            this.Load += new System.EventHandler(this.NmapMT_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numThreads)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFrom1;
+        private System.Windows.Forms.TextBox txtFrom2;
+        private System.Windows.Forms.TextBox txtFrom3;
+        private System.Windows.Forms.TextBox txtFrom4;
+        private System.Windows.Forms.TextBox txtTo1;
+        private System.Windows.Forms.TextBox txtTo2;
+        private System.Windows.Forms.TextBox txtTo3;
+        private System.Windows.Forms.TextBox txtTo4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListView lvScans;
+        private System.Windows.Forms.ColumnHeader IP;
+        private System.Windows.Forms.ColumnHeader Results;
+        private System.Windows.Forms.Button btnStartStop;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.NumericUpDown numThreads;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar tstProgress;
+        private System.Windows.Forms.ToolStripStatusLabel tstStatus;
+    }
+}
+
---
+++ Nmap_MT/NmapMT.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Nmap_MT
+{
+    public partial class NmapMT : Form
+    {
+        private ScanList g_ScanList = null;
+
+        public NmapMT()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStartStop_Click(object sender, EventArgs e)
+        {
+            groupBox1.Enabled = groupBox2.Enabled = groupBox3.Enabled = groupBox4.Enabled = btnStartStop.Text != "Start";
+            if (btnStartStop.Text == "Start")
+            {
+                btnStartStop.Text = "Stop";
+                btnStartStop.ForeColor = Color.Red;
+                if (g_ScanList != null && MessageBox.Show(this,"Do you want to resume last scan?", "Restore Last Scan", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.No)
+                {
+                    g_ScanList = null;
+                }
+
+                if (g_ScanList == null)
+                {
+                    int from1 = 0, from2 = 0, from3 = 0, from4 = 0, to1 = 0, to2 = 0, to3 = 0, to4 = 0;
+                    if (!ValidateIPRange(ref txtFrom1, ref txtTo1, ref from1, ref to1) || !ValidateIPRange(ref txtFrom2, ref txtTo2, ref from2, ref to2) || !ValidateIPRange(ref txtFrom3, ref txtTo3, ref from3, ref to3) || !ValidateIPRange(ref txtFrom4, ref txtTo4, ref from4, ref to4))
+                    {
+                        MessageBox.Show("Please check TO & FROM Addresses!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        btnStartStop.Text = "Start";
+                        btnStartStop.ForeColor = Color.Green;
+                        groupBox1.Enabled = groupBox2.Enabled = groupBox3.Enabled = groupBox4.Enabled = true;
+                        return;
+                    }
+                    tstStatus.Text = "Generating ScanList.xml";
+
+                    List<ScanListHost> generatedHosts = new List<ScanListHost>();
+
+                    int hostCount = 0;
+                    for (int a = from1; a <= to1; a++)
+                    {
+                        for (int b = from2; b <= to2; b++)
+                        {
+                            for (int c = from3; c <= to3; c++)
+                            {
+                                for (int d = from4; d <= to4; d++)
+                                {
+                                    hostCount++;
+                                }
+                            }
+                        }
+                    }
+
+                    int currCount = 0;
+                    for (int a = from1; a <= to1; a++)
+                    {
+                        for (int b = from2; b <= to2; b++)
+                        {
+                            for (int c = from3; c <= to3; c++)
+                            {
+                                for (int d = from4; d <= to4; d++)
+                                {
+                                    currCount++;
+                                    tstProgress.Value = ((currCount * 100) / hostCount);
+                                    generatedHosts.Add(new ScanListHost
+                                    {
+                                        IP = $"{a}.{b}.{c}.{d}",
+                                        ScanResult = string.Empty
+                                    });
+                                    if (currCount % 255 == 0)
+                                        Application.DoEvents();
+                                    if (btnStartStop.Text == "Start")
+                                    {
+                                        tstStatus.Text = "Idle..";
+                                        tstProgress.Value = 0;
+                                        return;
+                                    }                                        
+                                }
+                            }
+                        }
+                    }
+
+                    g_ScanList = new ScanList();
+                    g_ScanList.Host = generatedHosts.ToArray();
+
+                    tstStatus.Text = "Saving ScanList.xml..";
+                    SerializeObject(g_ScanList, "ScanList.xml");
+                    tstStatus.Text = "Saving ScanList.xml..done..";
+                }
+
+                if (g_ScanList.Host.Count() > 0)
+                {
+                    int scanned = g_ScanList.Host.Count(h => h.ScanResult != string.Empty);
+                    tstStatus.Text = $"{scanned} of {g_ScanList.Host.Count()} Hosts Scanned!";
+                }
+
+                tstProgress.Value = 0;
+            }
+            else
+            {
+                btnStartStop.Text = "Start";
+                btnStartStop.ForeColor = Color.Green;
+            }
+        }
+
+        private bool ValidateIPRange(ref TextBox t1,ref TextBox t2, ref int from, ref int to)
+        {
+            if(string.IsNullOrEmpty(t1.Text) || string.IsNullOrEmpty(t2.Text))
+            {
+                return false;
+            }
+
+            int t1_val, t2_val;
+            if(int.TryParse(t1.Text, out t1_val) && int.TryParse(t2.Text, out t2_val))
+            {
+                if (t1_val < 0 || t2_val <0 || t1_val > 255 || t2_val > 255)
+                    return false;
+
+                if (t2_val < t1_val)
+                    return false;
+
+                from = t1_val;
+                to = t2_val;
+            }
+            else
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private void NmapMT_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("ScanList.xml"))
+            {
+                if (MessageBox.Show("Do you want to restore last scan parameters?", "Restore Last Scan?", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+                {
+                    XmlSerializer inSrlz = new XmlSerializer(typeof(ScanList));
+                    StreamReader inRead = new StreamReader("ScanList.xml");
+                    g_ScanList = ((ScanList)inSrlz.Deserialize(inRead));
+                    inRead.Close();
+
+                    ScanListHost start = g_ScanList.Host.First();
+                    ScanListHost end = g_ScanList.Host.Last();
+
+                    get_octets(start.IP, ref txtFrom1, ref txtFrom2, ref txtFrom3, ref txtFrom4);
+                    get_octets(end.IP, ref txtTo1, ref txtTo2, ref txtTo3, ref txtTo4);
+                }
+            }
+        }
+
+        private bool get_octets(string IP, ref TextBox oct1, ref TextBox oct2, ref TextBox oct3, ref TextBox oct4)
+        {
+            ScanFormatted sf = new ScanFormatted();
+            sf.Parse(IP, "%d.%d.%d.%d");
+            if(sf.Results.Count == 4)
+            {
+                oct1.Text = sf.Results[0].ToString();
+                oct2.Text = sf.Results[1].ToString();
+                oct3.Text = sf.Results[2].ToString();
+                oct4.Text = sf.Results[3].ToString();
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Serializes class to XML file
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="serializableObject">Class to serialize</param>
+        /// <param name="fileName">XML file path</param>
+        private static void SerializeObject<T>(T serializableObject, string fileName)
+        {
+            if (serializableObject == null) { return; }
+
+            XmlDocument xmlDocument = new XmlDocument();
+            XmlSerializer serializer = new XmlSerializer(serializableObject.GetType());
+            using (MemoryStream stream = new MemoryStream())
+            {
+                serializer.Serialize(stream, serializableObject);
+                stream.Position = 0;
+                xmlDocument.Load(stream);
+                xmlDocument.Save(fileName);
+                stream.Close();
+            }
+        }
+        
+        private void ip_change(object sender, KeyEventArgs e)
+        {
+            g_ScanList = null;
+        }
+    }
+}
