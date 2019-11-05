1f89272297f242fbd07593f2b8cfe9fb7637c7a1
ashvin.bhuttoo <ashvin.bhuttoo@gmail.com>
2019-11-05 15:51:32 +0400

    installer and program files fix

--- Nmap_MT/Program.cs
+++ Nmap_MT/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Security.AccessControl;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,8 +16,21 @@ namespace Nmap_MT
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            if (args.Length == 1 && args[0] == "INSTALLER") {
+                System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+                FileSystemAccessRule fsar = new FileSystemAccessRule("Users", FileSystemRights.FullControl, AccessControlType.Allow);
+                DirectorySecurity ds = null;
+                
+                ds = di.GetAccessControl();
+                ds.AddAccessRule(fsar);
+                di.SetAccessControl(ds);
+
+                Process.Start(Application.ExecutablePath); 
+                return; 
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new NmapMT());
